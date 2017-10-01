deb6aa364eba0b2b0a956e33fe3ff4b5b8284fe3
JAkerstrom <johanna_akerstrom@hotmail.com>
2017-10-01 11:05:22 +0200

    lite ändringar på story och actions

---
+++ Player.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel
+{
+    public static class Player
+    {
+
+        public static string Name { get; set; }
+        public static Room CurrentLocation { get; set; }
+        static List<Item> Inventory = new List<Item>();
+
+        /*Denna metod kommer att användas varje gång användaren skriver ner några instruktioner.
+         * Nyckel ord för giltliga instructioner är:
+         * GO (Direction)
+         * USE (Item) ON (Item/Door)
+         * TAKE (Item)
+         * DROP (Item)
+         * INSPECT (Item/Door)
+         * LOOK
+         */
+        public static void Action(string Instructions)
+        {
+            string[] usedKeywords = Instructions.Split(' ');
+
+            if (usedKeywords[0].Contains("go"))
+            {
+                foreach (var exit in CurrentLocation.exits)
+                {
+                    if (usedKeywords[1] == exit.Direction && (exit.Islocked == false))
+                    {
+                        CurrentLocation = exit.Destination;
+                        Console.WriteLine("You are now entering " + CurrentLocation.RoomName);
+                        break;
+                    }
+                    else if(usedKeywords[1] == exit.Direction && (exit.Islocked == true))
+                    {
+                        Console.WriteLine("The door to the {0} is locked, do you have a key? (check inventory)", exit.Direction);
+                        break;
+                    }
+                    else if(!(usedKeywords[1] == exit.Direction))
+                    {
+                        Console.WriteLine("There is no exit in that direction");
+                    }
+                }
+            }
+            else if (usedKeywords[0].Contains("use"))
+            {
+                for (int i = 0; i < Inventory.Count; i++)
+                {
+                    if (Inventory[i].Name == usedKeywords[1])
+                    {
+                        if (usedKeywords[2] == "on")
+                        {
+                            for (int j = 0; j < Inventory.Count; j++)
+                            {
+                                if (Inventory[i].Name == Inventory[j].Name)
+                                {
+                                    Console.WriteLine("You put the two pieces together, now its one key.");
+                                    Inventory.Add(new Item("key", "this is the key to the mayors office"));
+                                    Inventory.RemoveAt(i);
+                                    Inventory.RemoveAt(j);
+                                }
+                            }
+
+                            if (usedKeywords[3] == "door")
+                            {
+                                if (CurrentLocation.RoomName == "Mainhall")
+                                {
+                                    for (int e = 0; e < CurrentLocation.exits.Count; e++)
+                                    {
+                                        if (CurrentLocation.exits[e].Islocked == true)
+                                        {
+                                            CurrentLocation.exits[e].Islocked = false;
+                                            CurrentLocation = CurrentLocation.exits[e].Destination;
+                                            Console.WriteLine("The door is now unlocked and You are now entering " + CurrentLocation.RoomName);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if (usedKeywords[0].Contains("take"))
+            {
+                for (int i = 0; i < CurrentLocation.items.Count; i++)
+                {
+                    if (usedKeywords[1] == CurrentLocation.items[i].Name)
+                    {
+                        Console.WriteLine("Your picked up the " + CurrentLocation.items[i].Name);
+                        Inventory.Add(CurrentLocation.items[i]);
+                        CurrentLocation.items.RemoveAt(i);
+                    }
+                }
+            }
+            else if (usedKeywords[0].Contains("drop"))
+            {
+                for (int i = 0; i < Inventory.Count; i++)
+                {
+                    if (usedKeywords[1] == Inventory[i].Name)
+                    {
+                        Console.WriteLine("Your dropped the " + Inventory[i].Name);
+                        CurrentLocation.items.Add(Inventory[i]);
+                        Inventory.RemoveAt(i);
+                    }
+                }
+            }
+            else if (usedKeywords[0].Contains("inspect"))
+            {
+                for (int i = 0; i < CurrentLocation.items.Count; i++)
+                {
+                    if (usedKeywords[1] == CurrentLocation.items[i].Name)
+                    {
+                        Console.WriteLine(CurrentLocation.items[i].Description);
+                    }
+                }
+            }
+            else if (usedKeywords[0].Contains("look"))
+            {
+                Console.WriteLine(CurrentLocation.Description);
+            }
+            else Console.WriteLine("Invalid input, try again");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Room library = new Room("The library", "You see a big old library, there are lots of dusty bookshelves. In the corner there is a fire extinguisher");
+            library.items.Add(new Item("extinguisher", "It looks heavy"));
+            library.items.Add(new Item("broken-key", "lower piece of broken key"));
+
+            Room mainHall = new Room("Main hall", "There are three doors, to the west, east, and north. The Door to the north is blocked with large boards.");
+
+            Room secondFloor = new Room("Second floor", "You are on the second floor, the room is dark. There is a dusty table in the middle of the" +
+                                        "room, you see a small item lying on it, it looks like a key");
+            secondFloor.items.Add(new Item("broken-key", "upper piece of broken key"));
+            secondFloor.items.Add(new Item("shotgun", "Contains a shotgun"));
+
+            Room mayorsOffice = new Room("Mayors office", "You are in the mayors office");
+
+
+
+            library.exits.Add(new Exit("north", secondFloor, false));
+            library.exits.Add(new Exit("east", mainHall, false));
+
+            mainHall.exits.Add(new Exit("west", library, false));
+            mainHall.exits.Add(new Exit("east", mayorsOffice, true));
+
+            secondFloor.exits.Add(new Exit("west", library, false));
+
+            mayorsOffice.exits.Add(new Exit("east", mainHall, false));
+
+
+            Player.CurrentLocation = mainHall;
+
+
+            Console.WriteLine("Game -The dark scary house with a monster");
+            Console.Write("Enter your name: ");
+            Player.Name = Console.ReadLine();
+
+            Console.WriteLine("Ok " + Player.Name + ". You just woke up in a dark hallway in what looks like a big house. You are suffering from memory loss.");
+
+            while (true)
+            {
+                Console.Write("What is your action?: ");
+                Console.WriteLine("Type: \n -Look\n -Go \\north\\south\\east\\west \n -Inspect");
+                Player.Action(Console.ReadLine().ToLower());
+                Console.Clear();
+            }
+        }
+    }
+}
